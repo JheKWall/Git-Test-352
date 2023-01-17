b887b2e4a32fd011e4bf010481fe5a32c143e1c5
Kenneth Wallace <jhekwall@ut.utm.edu>
2023-01-17 13:48:53 -0600

    Hello World!

--- Classwork 2 - Getting Into Git - JKW - 352/Program.cs
+++ Classwork 2 - Getting Into Git - JKW - 352/Program.cs
@@ -10,6 +10,8 @@ namespace Classwork_2___Getting_Into_Git___JKW___352
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
